7205f943851f0581ca42c1a0d7617b1801dc7207
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-04-05 09:49:41 -0300

    chore: changed project from javascript to typescript

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+import { App } from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+
+root.render(<App />);
